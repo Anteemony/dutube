84e92f12342a50d6a82bb9473a2f62c04f4ef497
Anthony <Anteemony@gmail.com>
2025-03-22 17:03:23 +0100

    remove unwanted authorized user

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,7 @@ class Config:
 
     AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
-    AUTH_USERS = [BOT_OWNER, 6841592369] + (
+    AUTH_USERS = [BOT_OWNER] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
         if AUTH_USERS_TEXT
         else []
